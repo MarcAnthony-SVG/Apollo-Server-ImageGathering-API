1c14e7979fcc90e4555b190ff8a9b335577defd7
Marc Rodriguez <marcanthonyrodriguez95@gmail.com>
2021-03-17 16:10:49 -0500

    Update

--- API/graphql.js
+++ API/graphql.js
@@ -1,32 +1,32 @@
-const { ApolloServer } = require('apollo-server');
-// const { ApolloServerPluginInlineTrace } = require('apollo-server-core');
-const { TwitterAPI } = require('./datasources/index.js');
-const { typeDefs } = require('./schemas/index.gql');
-const { resolvers } = require('./resolvers/index.js');
+// const { ApolloServer } = require('apollo-server');
+// // const { ApolloServerPluginInlineTrace } = require('apollo-server-core');
+// const { TwitterAPI } = require('./datasources/index.js');
+// const { typeDefs } = require('./schemas/index.gql');
+// const { resolvers } = require('./resolvers/index.js');
 
-require('dotenv').config();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  // plugins: [ApolloServerPluginInlineTrace()],
-  dataSources: () => {
-    return {
-      twitterAPI: new TwitterAPI(),
-    };
-  },
-  context: () => {
-    return {
-      Token: process.env.Token,
-      Authorization: process.env.BEARER_TOKEN,
-    };
-  },
-});
-
-// const config = {
-//   api: {
-//     bodyParser: false,
+// require('dotenv').config();
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   // plugins: [ApolloServerPluginInlineTrace()],
+//   dataSources: () => {
+//     return {
+//       twitterAPI: new TwitterAPI(),
+//     };
 //   },
-// };
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+//   context: () => {
+//     return {
+//       Token: process.env.Token,
+//       Authorization: process.env.BEARER_TOKEN,
+//     };
+//   },
+// });
+
+// // const config = {
+// //   api: {
+// //     bodyParser: false,
+// //   },
+// // };
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
